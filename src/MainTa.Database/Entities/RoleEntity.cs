69e8d72fef27dabbc5d3b2fb2c0780329b4a8d6a
TrevorSomething675 <soalone999@mail.ru>
2023-12-13 14:36:29 +0300

    Выпил неактуальный проект

--- src/MainTa.Database/Entities/RoleEntity.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace MainTz.Database.Entities
-{
-    public class RoleEntity : BaseEntity
-	{
-        public string RoleName { get; set; }
-        public ICollection<UserEntity?> User { get; set; }
-    }
-}
