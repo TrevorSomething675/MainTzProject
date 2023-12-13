99498dab800079f73cefdb80d87f73d0a02775b9
TrevorSomething675 <soalone999@mail.ru>
2023-12-13 14:46:10 +0300

    Исправил ветку

---
+++ src/MainTz.Web/Program.cs
@@ -0,0 +1,20 @@
+using NLog.Web;
+
+namespace MainTz.Web
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                })
+            .UseNLog();
+    }
+}
